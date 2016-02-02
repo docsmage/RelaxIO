86ac66f3ae4bce5f0a032d551698bd6ee212f748
Renee CS <voiceofrae@gmail.com>
2016-02-01 17:49:06 -0800

    Fixes #3 Fixes #4 Fixes #5

--- dist/scripts/services/SoundService.js
+++ dist/scripts/services/SoundService.js
@@ -1,39 +1,38 @@
 relaxIO.factory("SoundService", function () {
 	
+	// holds all sounds
 	var sounds = sounds;
-	// I have no idea if this is helpful
-	var allSoundsPlaying = {};
+	// should only hold sounds currently playing
+	var allSoundsLoaded = {};
 	
 	return {
 		
 		// load sounds
 		loadSound: function (sound) {
 			
-			if (!allSoundsPlaying[sound.name]) {
-				allSoundsPlaying[sound.name] = new buzz.sound(sound.audioUrl, {
+			if (!allSoundsLoaded[sound.name]) {
+				allSoundsLoaded[sound.name] = new buzz.sound(sound.audioUrl, {
 					formats: [ 'mp3' ],
-					preload: true
+					preload: true,
+					loop: true
 				});
 			}
 			
 		},
-
 		
 		// determines whether to play or pause a sound
 		playOrPause: function (sound) {		
 			this.loadSound(sound);
-			
-			var soundFile = allSoundsPlaying[sound.name];
-			
+			var soundFile = allSoundsLoaded[sound.name];
 			soundFile.togglePlay();
-			
+			// switches between play or pause
 		},
 		
 		// needed for HTML to switch between play & pause buttons
 		isPlayingSound: function (sound) {
-			var soundFile = allSoundsPlaying[sound.name];
-			
-			return soundFile !== undefined && soundFile.playing;
+			var soundFile = allSoundsLoaded[sound.name];
+			//debugger;
+			return soundFile !== undefined && !soundFile.isPaused();
 		}
 		
 	}; // ends return
