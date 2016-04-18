4d8e0d0ae8ff58397ad6ae6a2419d62ca19bfe8c
Renee CS <voiceofrae@gmail.com>
2016-04-18 16:42:38 -0700

    Fixes #33 - replaced alerts with popups

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var Hapi = require('hapi'),
     path = require('path'),
-    port = process.env.PORT || 3000,
+    port = process.env.PORT || 8000,
     server = new Hapi.Server(port),
     routes = {
         css: {
