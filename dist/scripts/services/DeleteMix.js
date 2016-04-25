8525a3282638f07fae2d90052d5d77380e1d652a
Renee CS <voiceofrae@gmail.com>
2016-04-24 21:44:15 -0700

    alert added prompting user to confirm deletion

--- dist/scripts/services/DeleteMix.js
+++ dist/scripts/services/DeleteMix.js
@@ -6,16 +6,16 @@ relaxIO.factory("DeleteMix", ["SoundService", "Session", function (SoundService,
 	return {
 		
 		deleteMix: function (mix) {
-		// TODO: add alert that asks "Are you sure you want to delete {{mix.name}}?" Which requires an 'OK' and has a Cancel option
+
+			var r = confirm("Are you sure you want to delete this mix?");
 			
-			debugger;
+			if (r == true) {
+				var mix = {name: name};
+				ref.child("/user/" + Session.getUserId() + "/mixes/" + mix).remove();
+			} else {
+				return;
+			}
 			
-			// DON'T CHANGE THIS
-			var mix = {
-				name: name};
-			
-			ref.child("/user/" + Session.getUserId() + "/mixes/" + mix).remove();
-				
 		}
 		
 	}
