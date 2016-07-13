ea2e6d48c80dc5e4cfa99795d82f47cf1491e015
Eugen Istoc <eugenistoc@gmail.com>
2016-07-13 14:50:13 -0400

    Reafactor to use Firebase >3.x
    Firebase api changed drastically between 2.x and 3.x. I rafactored using latest api docs. In `index.html` you will need to update the firebase initcode on line 80 to reflect your firebase app details.

--- app/scripts/services/SaveMix.js
+++ app/scripts/services/SaveMix.js
@@ -1,33 +1,33 @@
-relaxIO.factory("SaveMix", ["SoundService", "Session", function (SoundService, Session) {
-	
-	var ref = new Firebase("https://relaxio.firebaseio.com/users");
-	
+relaxIO.factory("SaveMix", ["SoundService", "Session", function (SoundService, $firebaseObject, Session) {
+
+	var ref = firebase.database().ref();
+
 	return {
-		
+
 		saveMix: function (name) {
-			
+
 			// save mix name
 			var mix = {
 				name: name,
-				
+
 				// compile sounds & volumes
 				sounds: SoundService.getCurrentlyPlaying()
 			};
-			
+
 			// save to 'mixes' area
 			ref.child("/user/" + Session.getUserId() + "/mixes/" + name).set(mix);
 		},
-		
+
 		// load saved mixes
-		getMixes: function (callback) {				
-		// grabbing user info	
+		getMixes: function (callback) {
+		// grabbing user info
 			ref.child("user").orderByKey().equalTo(Session.getUserId()).on("value", function(snapshot) {
 				var user = snapshot.val();
-			
+
 				callback(user[Object.keys(user)[0]].mixes);
 			}, function (errorObject) {
 			});
 		}
-	
+
 	}
 }]);
